d02650280ff57b6f1e94c89f30c58bb87fa952dc
jatinrathod1125 <jmrathod1125@gmail.com>
2026-01-06 12:29:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
-  server: {
-    host: '0.0.0.0',
-    port: 5174,
-    strictPort: false,
-    allowedHosts: ['demolaravel.lv.pwtech.pw'], 
-    cors: true,
-    hmr: true,
-  },
-});
+  base: '/portfolio/', // 🔥 IMPORTANT
+})
